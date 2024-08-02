fedf60fc009b77af01284fd59320264f65aafa18
sy-nguyen <syvipko42@gmail.com>
2024-08-02 16:48:40 +0700

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,33 +1,64 @@
-import React, {useContext, useState} from 'react'
-import { AppContext } from '../../AppContext'
-import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap'
-import { Link} from 'react-router-dom'
+import React, {useState} from "react";
+import {Box , AppBar, Toolbar, Container, Typography, MenuItem, Drawer } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu";
+import { BsCart3 } from 'react-icons/bs';
+import logo from '../../Images/logo_L_nobg.png';
+import "./header.css"
+
 
 export default function Header() {
-  const [collapsed, setCollapsed] = useState(true)
-  const toggleNavbar = () => setCollapsed(!collapsed)
-  const {cart} = useContext(AppContext)
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
   return (
     <div>
-      <div>
-      <Navbar color="faded" light expand="md">
-        <NavbarBrand href="/" className="me-auto">
-          reactstrap
-        </NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="me-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="/product">Product</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/register">Register</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-        <Link to="/cart">Cart <span>{cart.length}</span></Link>
-      </Navbar>
-      </div>
+      <AppBar position="fixed" sx={{ bgcolor: "transparent"}}>
+        <Container maxWidth="lg">
+          <Toolbar sx={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <img className="logo" src={logo} alt="logo" />
+            </Box>
+            <Box sx={{ display: { xs: "none", md: "flex" }, gap: 2 }}>
+              <MenuItem>
+                <Typography variant="body2" color="text.primary">Trang chủ</Typography>
+              </MenuItem>
+              <MenuItem>
+                <Typography variant="body2" color="text.primary">Sản phẩm</Typography>
+              </MenuItem>
+              <MenuItem>
+                <Typography variant="body2" color="text.primary">Blog</Typography>
+              </MenuItem>
+              <MenuItem>
+                <Typography variant="body2" color="text.primary">Liên hệ</Typography>
+              </MenuItem>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <BsCart3 style={{ fontSize: '1.5rem', marginLeft: 2 }} />
+            </Box>
+            <Box sx={{ display: { sm: "flex", md: "none" } }}>
+              <MenuIcon onClick={toggleDrawer(true)} />
+              <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                <Box sx={{ width: 250 }}>
+                  <MenuItem>
+                    <Typography variant="body2">Trang chủ</Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <Typography variant="body2">Sản phẩm</Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <Typography variant="body2">Blog</Typography>
+                  </MenuItem>
+                  <MenuItem>
+                    <Typography variant="body2">Liên hệ</Typography>
+                  </MenuItem>
+                </Box>
+              </Drawer>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </div>
-  )
+  );
 }
---
+++ src/components/notfound/Test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './test.css'
+
+export default function Test() {
+  return (
+    <div className='test'>Test</div>
+  )
+}
